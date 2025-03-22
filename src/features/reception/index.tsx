f859eacd0399211132eee59d0b9ce12aaf4a6e70
Sean <sean.parker@x-labsystems.co.uk>
2025-03-22 15:50:08 +0000

    Reception with use a box if the screensize is a phone

--- src/features/reception/index.tsx
+++ src/features/reception/index.tsx
@@ -1,20 +1,27 @@
-﻿import { Card, Container, Group, Title, Text, Image } from "@mantine/core";
+﻿import { Card, Container, Group, Title, Text, Image, rem, Badge, em, Box } from "@mantine/core";
 import reception from "../../assets/reception/greatLodge.jpg";
 import classes from "../../global.module.css";
+import localClasses from "./reception.module.css";
+import { useMediaQuery } from "@mantine/hooks";
 export default function ReceptionPage(){
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`);
+  const badgeSize = isMobile ? "sm" : "lg";
+
+  const badge = isMobile ? (<Box className={localClasses.mobileBadge}>Address: Braintree Rd, Great Bardfield, Braintree CM7 4QD</Box>)
+    : (<Badge className={localClasses.badge} size={badgeSize}>Address: Braintree Rd, Great Bardfield, Braintree CM7 4QD</Badge>)
   return(
     <Container>
-      <Title className={classes.title}>Reception</Title>
+      <Title className={classes.title} size={rem(60)}>Anne of Cleves Barn</Title>
       <Card shadow="sm" padding="lg" radius="md" withBorder>
         <Card.Section>
           <Image
             src={reception}
-            height={160}
+            height={500}
           />
         </Card.Section>
 
-        <Group justify="space-between" mt="md" mb="xs">
-          <Text fw={500}>Reception</Text>
+        <Group justify="flex-end" mt="md" mb="xs">
+          {badge}
         </Group>
 
         <Text size="sm" c="dimmed">
