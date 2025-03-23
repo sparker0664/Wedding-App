d2ab6ad1b8f66ef59fdb801f3da9d609c99ab234
Sean <sean.parker@x-labsystems.co.uk>
2025-03-23 11:05:20 +0000

    Added the taxi page

---
+++ src/components/IconLabelLink/index.tsx
@@ -0,0 +1,29 @@
+﻿import { ReactNode } from "react";
+import { Box, Group, Text } from "@mantine/core";
+import classes from "./iconLabelLink.module.css";
+interface IconLabelLinkProps {
+  icon: ReactNode;
+  label: string;
+  name: string;
+  url: string;
+}
+export default function IconLabelLink({
+  icon,
+  label,
+  name,
+  url,
+}: IconLabelLinkProps) {
+  return (
+    <Box>
+      <Group align="center">
+        <div style={{ width: 30 }}>{icon}</div>
+        <Text fw={700} className={classes.text}>
+          {label + `: `}
+        </Text>
+        <Text component={"a"} href={url} fw={700} className={classes.text}>
+          {name}
+        </Text>
+      </Group>
+    </Box>
+  );
+}
---
+++ src/features/taxis/components/taxiCompany/index.tsx
@@ -0,0 +1,64 @@
+﻿import { Card, Group, Stack, Title, Image } from "@mantine/core";
+import classes from "../../../../global.module.css";
+import localClasses from "./taxiCompany.module.css";
+import IconLabelText from "../../../../components/IconLabelText";
+import {
+  IconAddressBook,
+  IconClockHour5,
+  IconPhone,
+  IconWorld,
+} from "@tabler/icons-react";
+import IconLabelCopy from "../../../../components/IconLabelCopy";
+import IconLabelLink from "../../../../components/IconLabelLink";
+import IconLabelPhone from "../../../../components/IconLabelPhone";
+
+interface TaxiCompanyProps {
+  name: string;
+  phoneNumber: string;
+  address: string;
+  website: string;
+  operatingHours: string;
+  imageSrc: string;
+}
+export default function TaxiCompany({
+  name,
+  phoneNumber,
+  address,
+  operatingHours,
+  website,
+  imageSrc,
+}: TaxiCompanyProps) {
+  return (
+    <Card className={localClasses.card}>
+      <Card.Section>
+        <Title className={classes.title}>{name}</Title>
+      </Card.Section>
+      <Group>
+        <Stack mr="lg">
+          <IconLabelPhone
+            icon={<IconPhone className={localClasses.icon} />}
+            label="Phone"
+            phoneNumber={phoneNumber}
+          />
+          <IconLabelCopy
+            icon={<IconAddressBook className={localClasses.icon} />}
+            label="Address"
+            text={address}
+          />
+          <IconLabelText
+            icon={<IconClockHour5 className={localClasses.icon} />}
+            label="Operating Hours"
+            text={operatingHours}
+          />
+          <IconLabelLink
+            icon={<IconWorld className={localClasses.icon} />}
+            label="Website"
+            name={name}
+            url={website}
+          />
+        </Stack>
+        <Image height={100} width={100} src={imageSrc} />
+      </Group>
+    </Card>
+  );
+}
